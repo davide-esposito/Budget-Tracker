c5f97df81212ad81a8b5f94c67f3b22f7c8930bb
Davide Esposito <Davide@Micheles-MBP.fritz.box>
2024-11-11 16:59:39 +0100

    Add currency selection feature and update user settings

---
+++ app/wizard/_actions/userSettings.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { UpdateUserCurrencySchema } from "../../../schema/userSettings";
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function UpdateUserCurrency(currency: string) {
+  const parsedBody = UpdateUserCurrencySchema.safeParse({ currency });
+  if (!parsedBody.success) {
+    throw parsedBody.error;
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const userSettings = await prisma.userSettings.update({
+    where: {
+      userId: user.id,
+    },
+    data: {
+      currency,
+    },
+  });
+
+  return userSettings;
+}
