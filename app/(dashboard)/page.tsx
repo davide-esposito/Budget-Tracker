8856c11be2cf3043ce32e6eceadeff441b684fad
Davide Esposito <Davide@Micheles-MBP.fritz.box>
2024-11-14 15:57:22 +0100

    Add MAX_DATE_RANGE_DAYS constant and Overview component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,6 +4,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 import CreateTransactionDialog from "./_components/CreateTransactionDialog";
+import Overview from "./_components/Overview";
 
 export default async function page() {
   const user = await currentUser();
@@ -52,6 +53,7 @@ export default async function page() {
           </div>
         </div>
       </div>
+      <Overview userSettings={userSettings} />
     </div>
   );
 }
