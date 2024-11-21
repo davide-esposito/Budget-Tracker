3ad12278fc0e53c4cc513108190617167359aa70
Davide Esposito <Davide@Micheles-MBP.fritz.box>
2024-11-21 19:25:43 +0100

    finish manage page and styling

--- app/(dashboard)/_actions/categories.ts
+++ app/(dashboard)/_actions/categories.ts
@@ -4,6 +4,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import {
   CreateCategorySchema,
   CreateCategorySchemaType,
+  DeleteCategorySchema,
+  DeleteCategorySchemaType,
 } from "../../../schema/categories";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
@@ -26,6 +28,28 @@ export async function CreateCategory(form: CreateCategorySchemaType) {
       name,
       icon,
       type,
+    },
+  });
+}
+
+export async function DeleteCategory(form: DeleteCategorySchemaType) {
+  const parsedBody = DeleteCategorySchema.safeParse(form);
+  if (!parsedBody.success) {
+    throw new Error("bad request");
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  return await prisma.category.delete({
+    where: {
+      name_userId_type: {
+        userId: user.id,
+        name: parsedBody.data.name,
+        type: parsedBody.data.type,
+      },
     },
   });
 }
---
+++ app/(dashboard)/_components/DeleteCategoryDialog.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Category } from "@prisma/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { ReactNode } from "react";
+import { DeleteCategory } from "../_actions/categories";
+import { ca } from "date-fns/locale";
+import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { TransactionType } from "@/lib/types";
+interface Props {
+  trigger: ReactNode;
+  category: Category;
+}
+
+export default function DeleteCategoryDialog({ category, trigger }: Props) {
+  const categoryIdentifier = `${category.name}-${category.type}`;
+  const queryClient = useQueryClient();
+  const deleteMutation = useMutation({
+    mutationFn: DeleteCategory,
+    onSuccess: async () => {
+      toast.success(`Category deleted successfully 🎉`, {
+        id: categoryIdentifier,
+      });
+      await queryClient.invalidateQueries({
+        queryKey: ["categories"],
+      });
+    },
+    onError: () => {
+      toast.error("An error occurred while deleting the category", {
+        id: categoryIdentifier,
+      });
+    },
+  });
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to delete the category {category.name}?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              toast.loading(`Deleting category ${category.name}...`, {
+                id: categoryIdentifier,
+              });
+              deleteMutation.mutate({
+                name: category.name,
+                type: category.type as TransactionType,
+              });
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
