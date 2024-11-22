c90d5735e04fba822d3a7d709f9a3b52b2d0106e
Davide Esposito <Davide@Micheles-MBP.fritz.box>
2024-11-22 14:52:37 +0100

    Add @tanstack/react-table package and implement DataTableViewOptions and DataTableColumnHeader components

---
+++ components/datatable/ColumnToggle.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useMemo } from "react";
+import { Table } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MixerHorizontalIcon } from "@radix-ui/react-icons";
+
+interface DataTableViewOptionsProps {
+  table: Table<any>;
+}
+
+const DataTableViewOptions: React.FC<DataTableViewOptionsProps> = ({
+  table,
+}) => {
+  const columns = useMemo(
+    () =>
+      table
+        .getAllColumns()
+        .filter(
+          (column) =>
+            typeof column.accessorFn !== "undefined" && column.getCanHide()
+        ),
+    [table]
+  );
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          className="hidden h-8 ml-auto lg:flex"
+        >
+          <MixerHorizontalIcon className="w-4 h-4 mr-2" />
+          View
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[150px]">
+        <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {columns.map((column) => (
+          <DropdownMenuCheckboxItem
+            key={column.id}
+            className="capitalize"
+            checked={column.getIsVisible()}
+            onCheckedChange={(value) => column.toggleVisibility(!!value)}
+          >
+            {column.id}
+          </DropdownMenuCheckboxItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default DataTableViewOptions;
