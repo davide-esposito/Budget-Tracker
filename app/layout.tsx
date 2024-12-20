021f86e74b4bd963e108213ec1324cd9068950d9
Davide Esposito <Davide@6fae0fb8-e967-4ec7-a98b-350398ccce4d.fritz.box>
2024-12-20 15:21:27 +0100

    Update database configuration and dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider afterSignOutUrl="/sign-in">
+    <ClerkProvider
+      signInFallbackRedirectUrl={
+        process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL
+      }
+      signUpFallbackRedirectUrl={
+        process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL
+      }
+    >
       <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
         <body className="antialiased">
           <Toaster richColors position="bottom-right" />
